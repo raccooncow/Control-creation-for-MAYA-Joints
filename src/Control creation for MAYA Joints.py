682fd7a86ee99ac29b0aed83e38bc5671dacb0ee
Raquel Baca <raquelbaca4002@gmail.com>
2025-12-14 10:59:42 -0600

    Pseudocode for imports and creating a map for joints

---
+++ src/Control creation for MAYA Joints.py
@@ -0,0 +1,9 @@
+# Add imports
+
+# Create empty dict to store joint
+
+# Function remove_joint_ending(name)
+    # For loop for joints ending in JNT or jnt
+        # If joint ends with either
+            # Remove ending and return name
+    # If no ending, return og name
