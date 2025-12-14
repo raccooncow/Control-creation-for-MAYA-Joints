fa0f794a1275f3f5274bb3f1bbb0402e6b97249f
Raquel Baca <raquelbaca4002@gmail.com>
2025-12-14 11:18:02 -0600

    Freeze the changes, then delete history

--- src/Control creation for MAYA Joints.py
+++ src/Control creation for MAYA Joints.py
@@ -34,5 +34,6 @@ for jnt in joints:
     # Align group to joint
     cmds.delete(cmds.parentConstraint(jnt, grp, maintainOffset=False))
     # Zero out control
-
+    cmds.makeIdentity(ctrl, apply=True, translate=True, rotate=True, scale=True)
+    cmds.delete(ctrl, constructionHistory=True)
     # Control drives joint
