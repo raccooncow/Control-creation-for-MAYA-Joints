00aaf0bc9b9397e1f96539f56268c1172a200a79
Raquel Baca <raquelbaca4002@gmail.com>
2025-12-14 11:02:01 -0600

    Created empty dict, and defined function to remove name endings

--- src/Control creation for MAYA Joints.py
+++ src/Control creation for MAYA Joints.py
@@ -2,9 +2,13 @@
 import maya.cmds as cmds
 
 # Create empty dict to store joint
-
+joint_ctrl_map = {}
 # Function remove_joint_ending(name)
+def remove_joint_ending(name):
     # For loop for joints ending in JNT or jnt
+
         # If joint ends with either
+
             # Remove ending and return name
+
     # If no ending, return og name
