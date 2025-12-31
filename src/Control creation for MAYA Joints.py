4a85af3b6911a73de0ecd15540a92d5de4977a0e
Raquel Baca <raquelbaca4002@gmail.com>
2025-12-31 12:15:20 -0600

    Create cons and grps for jnt in chain, skip final jnt

--- src/Control creation for MAYA Joints.py
+++ src/Control creation for MAYA Joints.py
@@ -12,20 +12,22 @@ def remove_joint_ending(name):
 def get_joint_chain(root):
     chain = [root]
     current = root
-
     while True:
         children = cmds.listRelatives(current, children=True, type="joint") or []
         if not children:
             break
         current = children[0]
         chain.append(current)
-
     return chain
 
-for jnt in joints:
-    base_name = remove_joint_ending(jnt)
-    con_name = base_name + "_CON"
-    grp_name = base_name + "_GRP"
+for root in roots:
+    joint_chain = get_joint_chain(root)
+    joint_chain = joint_chain[:-1]
+    previous_control = None
+    for jnt in joint_chain:
+        base_name = remove_joint_ending(jnt)
+        con_name = base_name + "_CON"
+        grp_name = base_name + "_GRP"
     con = cmds.circle(
         name = con_name,
         normal = (1, 0, 0),
